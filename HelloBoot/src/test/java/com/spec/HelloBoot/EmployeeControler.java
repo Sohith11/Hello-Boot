9bd7ec1088b58f3576fd4b94b22dd4fa3f7f6cf0
Sohith11 <21bk1a66a9@stpetershyd.com>
2024-04-13 11:52:00 +0530

    intialize

---
+++ HelloBoot/src/test/java/com/spec/HelloBoot/EmployeeControler.java
@@ -0,0 +1,5 @@
+package com.spec.HelloBoot;
+
+public class EmployeeControler { 
+
+}
---
+++ HelloBoot/src/test/java/com/spec/HelloBoot/HelloController.java
@@ -0,0 +1,20 @@
+package com.spec.HelloBoot;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+@Controller
+@ResponseBody
+
+public class HelloController {
+
+ @RequestMapping("/hello")
+ public String sayHello() { 
+	 return "Hello SPEC..... !";
+ }
+@RequestMapping("/name")
+public String printName() { 
+	return " Sohith ";
+	}
+
+}
